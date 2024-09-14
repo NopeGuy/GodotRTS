29fd94ab88f2c82b12d92a1d838335153efd3462
Luís Ferreira <luisgmsferreira@gmail.com>
2024-09-14 02:00:14 +0100

    yay movement

--- Unit.gd
+++ Unit.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @onready var box = get_node("Box")
 @onready var collision_shape = get_node("CollisionShape2D")
 
+var currPos = Vector2(581, 292)  # Use Vector2 instead of array for position
+
 func _ready():
 	set_selected(selected)
 
@@ -17,3 +19,21 @@ func get_global_rect() -> Rect2:
 		var size = shape.extents * 2  # Full size from extents
 		return Rect2(global_position - size / 2, size)  # Use global_position
 	return Rect2()
+
+func _input(event):
+	# Handle input without rotating the sprite
+	if event.is_action_pressed("ui_right"):
+		currPos.x += 54  # Move right by 32 pixels
+		currPos.y += 29  # Move down by 32 pixels
+	elif event.is_action_pressed("ui_left"):
+		currPos.x -= 54  # Move left by 32 pixels
+		currPos.y -= 29  # Move up by 32 pixels
+	elif event.is_action_pressed("ui_up"):
+		currPos.y -= 29  # Move up by 32 pixels
+		currPos.x += 54  # Move right by 32 pixels
+	elif event.is_action_pressed("ui_down"):
+		currPos.y += 29  # Move down by 32 pixels
+		currPos.x -= 54  # Move left by 32 pixels
+
+	# Update the player's position without affecting rotation
+	self.position = currPos
